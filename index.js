5839471dc5972c0432166e628c958ccde893709d
Ignacio <ignacio@billeke.cl>
2023-07-10 16:41:00 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,39 @@
+/* Para inicar el proyecto se debe usar en cmd de node, ejecutarlo como administrador y colocar en consola npm run dv con el acceso al escritorio en la carpeta C:\Users\ignac\Desktop\Agencia de viajes */
+
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
+
+const app = express();
+
+// Conectar la base de datos
+db.authenticate()
+    .then( () => console.log('Base de datos conectada') )
+    .catch( error => console.log(error));
+
+// Definir puerto
+const port = process.env.PORT || 4000;
+
+// Habilitar pug
+app.set('view engine', 'pug');
+
+// Obtener el año actual
+app.use((req, res, next) => {
+    const year = new Date();
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombresitio = "Agencia de Viajes |";
+    next();    
+});
+
+// Agregar body parser para leer los datos del formulario
+app.use(express.urlencoded({extended: true}));
+
+// Definir la carpeta publica
+app.use(express.static('public'));
+
+// Agregar router
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import {
+    paginaInicio,
+    paginaNosotros,
+    paginaViajes,
+    paginaTestimoniales,
+    paginaDetalleViaje
+} from '../controllers/paginaController.js';
+import {
+    guardarTestimonial
+} from '../controllers/testimonialController.js'
+
+const router = express.Router();
+
+// req - lo que enviamos : res - lo que express nos responde
+router.get('/', paginaInicio);
+
+router.get('/nosotros', paginaNosotros);
+
+router.get('/viajes', paginaViajes);
+
+router.get('/viajes/:slug', paginaDetalleViaje);
+
+router.get('/testimoniales', paginaTestimoniales);
+router.post('/testimoniales', guardarTestimonial);
+
+export default router; 
